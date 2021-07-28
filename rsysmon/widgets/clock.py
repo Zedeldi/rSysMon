9b88d6aab3f887990292b3c34ccc8079a3be8424
Zedeldi <66186954+Zedeldi@users.noreply.github.com>
2021-07-28 14:57:36 +0100

    Add countdown widget

--- rsysmon/widgets/clock.py
+++ rsysmon/widgets/clock.py
@@ -1,6 +1,7 @@
 """Widgets to display time-related information."""
 
-from datetime import datetime
+import math
+from datetime import datetime, timedelta
 
 from rsysmon.widgets import Information
 
@@ -15,3 +16,29 @@ class Clock(Information):
         format_ is a string to pass to strftime.
         """
         super().__init__(lambda: datetime.now().strftime(format_))
+
+
+class Countdown(Information):
+    """
+    Display time until given date, prefixed by label.
+
+    If ms is True, include microseconds.
+    """
+
+    def __init__(
+        self, date: datetime, label: str = "Countdown: ", ms: bool = False
+    ) -> None:
+        """Initialise parent Information with countdown."""
+        super().__init__(lambda: f"{label}{self.get_countdown(date, ms)}")
+
+    @staticmethod
+    def get_countdown(date: datetime, ms: bool) -> timedelta:
+        """
+        Return timedelta for time until date.
+
+        Round up microseconds if ms is False.
+        """
+        delta = date - datetime.now()
+        if not ms:
+            delta = timedelta(seconds=math.ceil(delta.total_seconds()))
+        return delta
